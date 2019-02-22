2b825b0678500fd6ae43928d9330eaba95c3c3d4
Chance Smith <chancesmith97@hotmail.com>
2019-02-22 23:04:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(x)
+  x.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(x)
+  x.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(x)
+  x.sort
+
+end
