757495ed7665d8b534795e614b8287e12e9997d0
Chance Smith <chancesmith97@hotmail.com>
2019-02-22 23:58:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(x)
+  x.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(x)
+  x.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(x)
+  x.sort_by {|x| x.length}
+
+end
+
+def swap_elements(x)
+  x[1], x[2] = x[2], x[1]
+  x
+end
+  
+def reverse_array(x)
+  x.reverse 
+end 
+
+def kesha_maker(x)
+  new_arr = []
+  x.each {|name| new_arr << name[2] = "$"}
+  
+end 
+
+def find_a(x)
+  x.select {|letter| letter[0] == "a"}
+end 
+
+def sum_array(x)
+  x.inject {|sum, num| sum = sum + num}
+end 
+
+def add_s(x)
+  x.each_with_index {|word, i| word << "s" unless i == 1}
+end 
+
+
 
